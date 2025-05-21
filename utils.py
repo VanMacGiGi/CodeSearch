daccc62ce3cd5a20d4f098caf668e1bbe3cd08e9
Van Mac <macthikieuvan+dev@gmail.com>
2025-05-21 23:12:18 +0700

    fix: support work with python3.8

--- utils.py
+++ utils.py
@@ -1,27 +1,23 @@
 import logging
 import subprocess
+from typing import List
 
 from line import Line
 
 logger = logging.getLogger(__name__)
 
 
-def find_match_lines(pattern, extensions=['*'], dir='.', file=None):
+def find_match_lines(pattern, extensions=["*"], dir=".", file=None):
     command = f"grep -E -H -n --color=never '{pattern}'"
     if not file:
         name_patterns = [f"-name '*.{x}'" for x in extensions]
-        command = (
-            f"find {dir} {' '.join(name_patterns)} | "
-            f"xargs {command}"
-        )
+        command = f"find {dir} {' '.join(name_patterns)} | xargs {command}"
     else:
         command += f" {file}"
 
     logger.debug(f"Command: {command}")
 
-    result = subprocess.run(
-        command, shell=True, capture_output=True, text=True
-    )
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
 
     lines = []
     for line_text in result.stdout.splitlines(True):
@@ -39,7 +35,7 @@ def find_match_lines(pattern, extensions=['*'], dir='.', file=None):
     return lines
 
 
-def get_wrapper_block(block: list[Line], lang):
+def get_wrapper_block(block: List[Line], lang):
     """
     Extracts a full C++ control block that wraps the given block, the given
     block is a list of lines.
@@ -60,18 +56,18 @@ def get_wrapper_block(block: list[Line], lang):
             wrapper_block = lang.get_control_block(current, brief=True)
             # Check if wrapper block contains our target block by index
             if (
-                wrapper_block and
-                wrapper_block[0].index <= start_line.index and
-                wrapper_block[-1].index >= end_line.index
+                wrapper_block
+                and wrapper_block[0].index <= start_line.index
+                and wrapper_block[-1].index >= end_line.index
             ):
                 return wrapper_block
         elif lang.is_definition_start(current.content):
             wrapper_block = lang.get_definition_block(current, brief=True)
             # Check if wrapper block contains our target block by index
             if (
-                wrapper_block and
-                wrapper_block[0].index <= start_line.index and
-                wrapper_block[-1].index >= end_line.index
+                wrapper_block
+                and wrapper_block[0].index <= start_line.index
+                and wrapper_block[-1].index >= end_line.index
             ):
                 return wrapper_block
         current = current.get_prev()
